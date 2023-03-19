845aeba96c2c40eb3d0559a78a23cd94e3995540
Hannah Kwon <hkwon1@g.ucla.edu>
2023-03-18 19:55:03 -0700

    Sends an email when the stream crashes

--- TwitchStreamer/main.py
+++ TwitchStreamer/main.py
@@ -1,11 +1,21 @@
 from dotenv import load_dotenv
-
 from stream.dj import DJ
+from send_email import send_email_when_error
 
-REFRESH_VIDEO_QUEUE_INTERVAL = 5
 
-load_dotenv()  # take environment variables from .env.
+try:
+    REFRESH_VIDEO_QUEUE_INTERVAL = 5
 
-if __name__ == "__main__":
-    dj = DJ(REFRESH_VIDEO_QUEUE_INTERVAL)
-    dj.start_stream()
+    load_dotenv()  # take environment variables from .env.
+
+    if __name__ == "__main__":
+        dj = DJ(REFRESH_VIDEO_QUEUE_INTERVAL)
+        dj.start_stream()
+
+except:
+    print("There is an error!")
+    send_email_when_error()
+    
+        
+
+
