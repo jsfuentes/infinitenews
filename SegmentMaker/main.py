8db352c47e46b66f91eda3661e965fa2d653a7c9
Hannah Kwon <hkwon1@g.ucla.edu>
2023-03-21 14:27:58 -0700

    Add skipping generation audio argument

--- SegmentMaker/main.py
+++ SegmentMaker/main.py
@@ -9,17 +9,22 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('-s', '--segment', type=str, default="upgraded",
                         help='Segment type. Options are upgraded | bananaqa')
 
+    #creates audio, default value is False
+    parser.add_argument('-a', '--audio', action = "store_true",default = "False" ,help = "Generate Audio")
+
     args = parser.parse_args()
     my_segment = UpgradedSegment()
+    
     if args.segment == "bananaqa":
         my_segment = BananaSegment()
+
+    skip_audio = args.audio == True
 
     print("MY NAME IS", my_segment.eleven_voice_name)
 
@@ -83,10 +88,14 @@ if __name__ == '__main__':
 
     print("Advancing to audio generation for",
           len(scripts_to_generate), "scripts")
-    for (script_name, topic, script) in scripts_to_generate:
-        print("Generating audio for", topic)
-        audio_file = create_audio_file(script_name, script, name_of_voice=my_segment.eleven_voice_name)
-        print(audio_file)
+    
+    if skip_audio : 
+        print("With audio argument - audio will be generated!")
+        for (script_name, topic, script) in scripts_to_generate:
+            # audio_file = create_audio_file(script_name, script, name_of_voice=my_segment.eleven_voice_name)
+            print("Generating audio for", topic)
+    else :
+        print("By default, with no audio argument - audio will not be generated!")
 
     print("Finished!")
 
